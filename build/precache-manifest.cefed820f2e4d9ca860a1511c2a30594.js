8f5cc491d617a3cafd3b708e5e116dcc9ab68fd8
Abkeyev Daniyar <abkeyev@gmail.com>
2021-05-18 18:13:35 +0600

    some changes

---
+++ build/precache-manifest.cefed820f2e4d9ca860a1511c2a30594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5168ef23c0332e96bc0f7053f9a62d0",
+    "url": "/mortgage/test/index.html"
+  },
+  {
+    "revision": "9b7ec6182dd57663e0aa",
+    "url": "/mortgage/test/static/css/main.f57910f7.chunk.css"
+  },
+  {
+    "revision": "e9516e09f95fe0bc7bf7",
+    "url": "/mortgage/test/static/js/2.070e8020.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/mortgage/test/static/js/2.070e8020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7ec6182dd57663e0aa",
+    "url": "/mortgage/test/static/js/main.b4edd962.chunk.js"
+  },
+  {
+    "revision": "9d38f04a8f9b3619abc2",
+    "url": "/mortgage/test/static/js/runtime-main.04d44106.js"
+  }
+]);
