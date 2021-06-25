3e7176307f7bde972cebc20b7b4ef460c5beb767
Abkeyev Daniyar <abkeyev@gmail.com>
2021-06-25 13:33:06 +0600

    max value bugfix

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -588,9 +588,9 @@ const Order = (props: any) => {
                             /[^a-zA-Z0-9]/g,
                             ""
                           );
-                          if (s > 50000000) {
-                            countMinPay(period, "50000000", analys);
-                            setPrice("50000000");
+                          if (s > +priceMax) {
+                            countMinPay(period, priceMax, analys);
+                            setPrice(priceMax);
                           } else {
                             countMinPay(period, s.toString(), analys);
                             setPrice(s.toString());
@@ -743,8 +743,8 @@ const Order = (props: any) => {
                             /[^a-zA-Z0-9]/g,
                             ""
                           );
-                          if (s > 10000000) {
-                            setPay("10000000");
+                          if (s > +payMax) {
+                            setPay(payMax);
                           } else {
                             setPay(s.toString());
                           }
@@ -805,9 +805,9 @@ const Order = (props: any) => {
                             /[^a-zA-Z0-9]/g,
                             ""
                           );
-                          if (s > 300) {
-                            countMinPay("300", price, analys);
-                            setPeriod("300");
+                          if (s > +periodMax) {
+                            countMinPay(periodMax, price, analys);
+                            setPeriod(periodMax);
                           } else {
                             countMinPay(s.toString(), price, analys);
                             setPeriod(s.toString());
